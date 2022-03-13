8799020cf37f9a11d9ee868c0025880187f3c737
Christian Rudder <ringthebellpepper@gmail.com>
2022-03-13 11:25:54 -0400

    adding footer, which concludes my study

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
 import ErrorPage from './components/pages/ErrorPage';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,9 +16,13 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element = {<Home/>}/>
+        <Route path='/services' element = {<Services/>}/>
+        <Route path='/products' element = {<Products/>}/>
+        <Route path='/sign-up' element = {<SignUp/>}/>
         <Route path="*" element = {<ErrorPage/>}/>
       </Routes>
     </Router>
+    <Footer/>
     </>
     
   );
---
+++ react-website/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      I'm footer
+    </div>
+  );
+}
+
+export default Footer;
